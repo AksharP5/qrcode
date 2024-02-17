f8e06e9c5c32c3e9b9a594badbe2d482052745eb
Akshar Patel <aksharpatel1233@gmail.com>
2024-02-17 21:02:36 +0000

    (feat): Add qrcode

--- hw1.py
+++ hw1.py
@@ -6,10 +6,10 @@ import qrcode
 # Function to generate QR code
 def generate_qr_code(data):
     # Create a QRCode object with specified parameters
-    qr = qrcode.make(data)
-
-    # Make the QR code fit and generate the image
-    qr.make(fit=True)
+    qr = qrcode.QRCode()
+    # Add data to the QRCode object
+    qr.add_data(data)
+    qr.make()
     qr_img = qr.make_image(fill_color="black", back_color="white")
 
     # Return the generated image
@@ -23,5 +23,4 @@ qr_code_img = generate_qr_code(data_to_encode)
 
 # Display the QR code using Matplotlib
 plt.imshow(qr_code_img)
-plt.axis('off')
 plt.show()
